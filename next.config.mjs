fa33375c64ce1cfef2a5040ca480d4f81801e049
rafliogun49 <rafliogun49@gmail.com>
2025-02-12 00:07:00 +0700

    edit the next config, idk why

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    experimental: {
+      appDir: true, // Ensure compatibility with Next.js App Router
+    },
+    images: {
+      unoptimized: true, // Cloudflare Pages doesn't support Next.js Image Optimization
+    },
+  };
+  
+  export default nextConfig;
+  
