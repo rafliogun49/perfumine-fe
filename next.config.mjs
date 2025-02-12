2d9313d32c7553a788de950144748d0b810ade2c
rafliogun49 <rafliogun49@gmail.com>
2025-02-12 09:15:29 +0700

    changing next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
-const nextConfig = {
-    experimental: {
-      appDir: true, // Ensure compatibility with Next.js App Router
-    },
-    images: {
-      unoptimized: true, // Cloudflare Pages doesn't support Next.js Image Optimization
-    },
-  };
+const nextConfig = {};
   if (process.env.NODE_ENV === 'development') {
     await setupDevPlatform();
   }
